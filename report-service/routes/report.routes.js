67fea2ab79067770918fdf5fac620094a7b9c2b0
Maroua Cherki <cherkimaroua7@gmail.com>
2026-03-26 16:13:13 +0100

    Update report.routes.js

--- report-service/routes/report.routes.js
+++ report-service/routes/report.routes.js
@@ -1,23 +1,15 @@
+
 const router = require('express').Router();
-const {
-  getOverview,
-  getTasksByStatus,
-  getTasksByPriority,
-  getTasksByUser,
-  getProjectsByCategory,
-  getActivity,
-  getProjectReport
-} = require('../controllers/report.controller');
+const c = require('../controllers/report.controller');
 const { verifyToken } = require('../middleware/auth.middleware');
 
 router.use(verifyToken);
-
-router.get('/overview',              getOverview);
-router.get('/tasks-by-status',       getTasksByStatus);
-router.get('/tasks-by-priority',     getTasksByPriority);
-router.get('/tasks-by-user',         getTasksByUser);
-router.get('/projects-by-category',  getProjectsByCategory);
-router.get('/activity',              getActivity);
-router.get('/project/:id',           getProjectReport);
+router.get('/overview', c.getOverview);
+router.get('/tasks-by-status', c.getTasksByStatus);
+router.get('/tasks-by-priority', c.getTasksByPriority);
+router.get('/tasks-by-user', c.getTasksByUser);
+router.get('/projects-by-category', c.getProjectsByCategory);
+router.get('/activity', c.getActivity);
+router.get('/project/:id', c.getProjectReport);
 
 module.exports = router;
